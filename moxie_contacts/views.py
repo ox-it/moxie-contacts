b1e76dd91d22d2fb62da72471535b4307b57b63a
Martin Filliau <martin@filliau.com>
2013-04-18 11:11:13 +0100

    Handle client errors

--- moxie_contacts/views.py
+++ moxie_contacts/views.py
@@ -12,10 +12,16 @@ logger = logging.getLogger(__name__)
 
 class Search(ServiceView):
 
+    AUTHORIZED_MEDIUMS = ('phone', 'email')
+
     def handle_request(self):
         # 1. Request from Service
         self.q = request.args.get('q', None)
         self.medium = request.args.get('medium', None)
+        if not self.q or not self.medium:
+            abort(400, description="Parameters 'q' (query) and 'medium' ('phone' or 'email') are mandatory.")
+        if self.medium not in Search.AUTHORIZED_MEDIUMS:
+            abort(400, description="'medium' should be one of '{}'.".format(', '.join(Search.AUTHORIZED_MEDIUMS)))
         provider = ContactSearchService.from_context()
         results = provider.search(self.q, self.medium)
         return results
